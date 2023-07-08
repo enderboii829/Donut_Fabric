56c1c206f1688cf26896028f58c86f9a8020bd34
Vishruth Sharath <vishruthappi@gmail.com>
2023-07-07 22:21:10 -0700

    Initial commit

---
+++ src/main/java/net/sl33pypanda/donut/Donut.java
@@ -0,0 +1,16 @@
+package net.sl33pypanda.donut;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Donut implements ModInitializer {
+	public static final String MOD_ID = "donut";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/sl33pypanda/donut/DonutClient.java
@@ -0,0 +1,10 @@
+package net.sl33pypanda.donut;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DonutClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
