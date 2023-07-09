222a38806b10f956c2a599be54ea229b5bf178a2
Vishruth Sharath <vishruthappi@gmail.com>
2023-07-08 22:02:55 -0700

    bruh

--- src/main/java/net/sl33pypanda/donut/item/ModItems.java
+++ src/main/java/net/sl33pypanda/donut/item/ModItems.java
@@ -1,12 +1,28 @@
 package net.sl33pypanda.donut.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.sl33pypanda.donut.Donut;
 
 public class ModItems {
+    public static final Item DONUT = registerItem("donut", new Item(new FabricItemSettings()));
 
-
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries){
+        entries.add(DONUT);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Donut.MOD_ID, name), item);
+    }
 
     public static void registerModItems(){
         Donut.LOGGER.info("Registering mod items for" + Donut.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToIngredientTabItemGroup);
     }
 }
