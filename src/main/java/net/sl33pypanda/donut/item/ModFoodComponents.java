0c2bda0d38fed733e27aed7de9b032cab65a4f53
Vishruth Sharath <vishruthappi@gmail.com>
2023-11-12 12:04:58 -0800

    Items Dude Items!

--- src/main/java/net/sl33pypanda/donut/item/ModFoodComponents.java
+++ src/main/java/net/sl33pypanda/donut/item/ModFoodComponents.java
@@ -7,14 +7,16 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent DONUT = new FoodComponent.Builder().alwaysEdible().hunger(4)
-            .saturationModifier(0.25f).build();
+            .saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY,8400),100f)
+            .build();
     public static final FoodComponent CHRISTMAS_COOKIE = new FoodComponent.Builder().alwaysEdible().hunger(4)
             .saturationModifier(0.25f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 180),100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 8400),100f)
             .build();
     public static final FoodComponent SUGAR_COOKIE = new FoodComponent.Builder().alwaysEdible().hunger(4)
             .saturationModifier(0.25f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY,180),100f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH,240),100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY,8400),100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH,8400),100f)
             .build();
 }
